0d9322075d5c07e520fd273dfa66bb28062cf2a8
dev-herbert <soyzabeto@gmail.com>
2023-06-25 00:25:54 -0300

    comentando em todo o codigo
    essa atualização foi para comentar todo o codigo para que todos entendam e consiga entender o que fiz nessa aplicação

--- jogo.js
+++ jogo.js
@@ -1,52 +1,55 @@
-let jogar = document.getElementById("btn_button")
+//pegando os elementos no DOM
+let btn_jogar = document.getElementById("btn_button")
 let img_dado = document.getElementById("img_dado")
-
-
 let dado_audio = document.getElementById("box_audio")
-
 let text_number = document.getElementById("number_dado")
+
+//function que sorteará o numero
 function sorteio_dado (){
     
     
-    
-    let dados = Math.floor(Math.random() * 7);
+    let dados = Math.floor(Math.random() * 7);//variavel que sorteará o numero com o metodo random
 
+    //uma condição que verifica se o numero é engual a 0 e for troca ele pra 1
+    //não fiz isso com mim e max por que desse jeito é mas facil e fica mas entendivel para que começo agora
     if(dados == 0){
         dados = 1
     }
 
 
+    //passará o numero que saiu do random para o DOM no paragrafo
     text_number.textContent = dados
     
+    //vai trocar a imagem do dado para o sorteado
     img_dado.setAttribute("src", `dado/${dados}.png`)
+    //deixará o botão jogar desativado
+    btn_jogar.classList.remove("off")
+    
+    img_dado.classList.remove("animations")//remover animation na imagem
 
-    jogar.classList.remove("off")
-    img_dado.classList.remove("animations")
-
+    //pausará o barulho do dado casó aconteça de sair 
     dado_audio.pause()
-    dado_audio.removeAttribute("src", "dado-rolando.mp3")
 }
 
-
-jogar.addEventListener("click", function () {
-
-    let card_btn = document.getElementById("btn_jogo")
+// esse evento faz todo o efeito no dom ele que é responsavel por dar todo o efeito na parte da img
+btn_jogar.addEventListener("click", function () {
 
     //colocar efeito de rolagem
     img_dado.classList.add("animations")
 
     //deixar o botão jogar invisivel
-    jogar.classList.add("off")
+    btn_jogar.classList.add("off")
 
-    //colocar interrogação na imagem e onde aparece o texto numerico
+    //trocar a imagem sempre que o botão receber o evento click
     img_dado.src = `dado/0.png`
+
+    //trocar a imagem quando o botão receber um click
     text_number.textContent = "?"
 
     //colocando som de dado rolando
-    dado_audio.setAttribute("src", "dado-rolando.mp3")
     dado_audio.play()
     
-
+    //chamando a function sorteio_dado com um delay de 2 segundos
     setTimeout(sorteio_dado, 2000)
     
 })
